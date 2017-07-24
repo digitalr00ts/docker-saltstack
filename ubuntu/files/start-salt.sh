7a2b41e025240434821e8862dae3e5b490c0c3ac
Carlos Meza <carlos@digitalr00ts.com>
2017-07-24 02:55:59 -0700

    add ubuntu

---
+++ ubuntu/files/start-salt.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+salt-call --local tls.create_self_signed_cert CN="${HOSTNAME}" -l error && \
+	rm -f /var/log/salt/minion
+
+cat > /opt/salt/master.d/01-api.conf << EOF
+rest_cherrypy:
+  port: 8000
+  debug: True
+  ssl_crt: /etc/pki/tls/certs/${HOSTNAME}.crt
+  ssl_key: /etc/pki/tls/certs/${HOSTNAME}.key
+  log_error_file: /opt/log/api-error
+  log_access_file: /opt/log/api-access
+  debug: True
+EOF
+
+echo
+echo "$(python --version)"
+cat /root/requirements.txt
+echo
+echo 'Saltstack:'
+echo "    $(salt-master --version)"
+echo "    $(salt-syndic --version)"
+echo "    $(salt-api --version)"
+echo
+
+salt --versions-report
+
+salt-syndic --daemon --config-dir=/opt/salt &
+# api_log_file setting does not exist so setting log file here
+# 2016.11.6
+salt-api --daemon --config-dir=/opt/salt --log-file=/opt/log/api --log-file-level=debug &
+salt-master --config-dir=/opt/salt
+
