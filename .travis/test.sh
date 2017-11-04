2248cf1c2c28db72cf2e27312cfcfd3261b16a78
Carlos Meza <carlos@digitalr00ts.com>
2017-11-04 01:15:17 -0700

    change grains test

--- .travis/test.sh
+++ .travis/test.sh
@@ -10,5 +10,5 @@ docker-compose -f .travis/compose-${distro}.yaml ps && sleep 3
 #TEST
 docker-compose -f .travis/compose-${distro}.yaml exec saltmaster salt-run manage.status
 docker-compose -f .travis/compose-${distro}.yaml exec saltminion salt-call test.ping
-docker-compose -f .travis/compose-${distro}.yaml exec saltminion sh -c '[ "$(salt-call --master=$(hostname) grains.get --output=newline_values_only nodename)" = $(hostname) ]'
+docker-compose -f .travis/compose-${distro}.yaml exec saltminion salt-call grains.get oscodename
 curl -vk https://localhost:8000
