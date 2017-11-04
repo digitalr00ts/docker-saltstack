97438a8367dcc9fc0d9282ad76de66d44a549e92
Carlos Meza <carlos@digitalr00ts.com>
2017-11-04 00:33:17 -0700

    update to use minion in container

--- .travis/test.sh
+++ .travis/test.sh
@@ -8,7 +8,7 @@ docker-compose -f .travis/compose-${distro}.yaml up -d
 docker-compose -f .travis/compose-${distro}.yaml ps && sleep 3
 
 #TEST
-docker-compose -f .travis/compose-${distro}.yaml exec saltmaster sh -c 'salt-call --master=$(hostname) test.ping || salt-key --accept=$(hostname) --yes'
-docker-compose -f .travis/compose-${distro}.yaml exec saltmaster sh -c 'salt-call --master=$(hostname) test.ping'
-docker-compose -f .travis/compose-${distro}.yaml exec saltmaster sh -c '[ $(salt-call --master=$(hostname) grains.get --output=newline_values_only nodename) = $(hostname) ]'
+docker-compose -f .travis/compose-${distro}.yaml exec saltmaster salt-run manage.status
+docker-compose -f .travis/compose-${distro}.yaml exec saltminion salt-call test.ping
+docker-compose -f .travis/compose-${distro}.yaml exec saltminion sh -c '[ "$(salt-call --master=$(hostname) grains.get --output=newline_values_only nodename)" = $(hostname) ]'
 curl -vk https://localhost:8000
